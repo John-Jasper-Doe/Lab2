63bed7fbc758560c0e627589dcdd361f224b795c
John 'jasper' Doe <john.jasper.doe@gmail.com>
2019-04-12 02:04:23 +0300

    Modify filter.
    Optimized in accordance with NRVO.
    	modify:                   filter.cpp
    	modify:                   filter.h

--- filter.h
+++ filter.h
@@ -9,16 +9,14 @@ std::vector<ipaddr_t> filter(const std::vector<ipaddr_t> &addr, Args... args)
                 "filter gets 4 parameters max + ip_pool");
 
   std::vector<ipaddr_t> ip_filtered;
-  std::vector<uint16_t> tmp_vec;
   for (const auto &ip : addr) {
-    tmp_vec.clear();
-    tmp_vec = to_vector(ip);
+    auto tmp_vec = to_vector(ip);
 
     if (std::equal(a.begin(), a.end(), tmp_vec.begin()))
       ip_filtered.push_back(ip);
   }
 
-  return std::move(ip_filtered);
+  return ip_filtered;
 }
 
 std::vector<ipaddr_t> filter_any(const std::vector<ipaddr_t> &addr,
