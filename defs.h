673d44ffccbd4887c9bc6df1fff694fa9d1ef63a
John Doe <john.jasper.doe@gmail.com>
2020-05-12 20:22:20 +0300

    remove all files for review

--- defs.h
+++
@@ -1,8 +0,0 @@
-#include <string>
-#include <vector>
-#include <tuple>
-
-
-using ipaddr_str_t = std::vector<std::string>;
-using ip_liststr_t = std::vector<std::vector<std::string>>;
-using ipaddr_t = std::tuple<uint16_t, uint16_t, uint16_t, uint16_t>;
--- utils.h
+++
@@ -1,9 +0,0 @@
-#include <vector>
-#include <string>
-
-/** @brief  Split a string by the given character
- *  @param  str [IN] - text line
- *  @param  dev [IN] - divider
- *  @return Vector strings separated by a given character */
-std::vector<std::string> split(const std::string &str, char div);
-
